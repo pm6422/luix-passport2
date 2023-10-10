dee80550d11488d15936c99176f3951e4f959afe
louis <pm6422@126.com>
2023-10-10 18:30:56 +0800

    new

--- luix-passport-server/src/main/java/sample/PassportServerApplication.java
+++ luix-passport-server/src/main/java/sample/PassportServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableLuixSpringBootFramework
 public class PassportServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PassportServerApplication.class, args);
     }
-
 }
