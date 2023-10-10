17e83912ff8acba7a25f908638a7f41ab3bd3b03
pm6422 <pm6422@126.com>
2023-10-10 22:16:40 +0800

    re

--- luix-passport-resource-server/src/main/java/sample/config/ResourceServerConfig.java
+++ luix-passport-resource-server/src/main/java/sample/config/ResourceServerConfig.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2020-2022 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package sample.config;
 
 import org.springframework.context.annotation.Bean;
@@ -28,19 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
 public class ResourceServerConfig {
-
     // @formatter:off
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.securityMatcher("/messages/**")
+				.securityMatcher("/messages/**")
 				.authorizeHttpRequests()
-					.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read")
-					.and()
-			.oauth2ResourceServer()
+				.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read")
+				.and()
+				.oauth2ResourceServer()
 				.jwt();
 		return http.build();
 	}
 	// @formatter:on
-
 }
