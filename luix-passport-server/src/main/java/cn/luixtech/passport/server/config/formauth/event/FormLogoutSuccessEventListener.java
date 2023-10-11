153f81a92445e8963ed68a7c394c1279669014b3
louis <pm6422@126.com>
2023-10-11 14:54:06 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/formauth/event/FormLogoutSuccessEventListener.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/formauth/event/FormLogoutSuccessEventListener.java
@@ -2,6 +2,8 @@ package cn.luixtech.passport.server.config.formauth.event;
 
 import jakarta.servlet.http.HttpSessionEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
@@ -11,6 +13,7 @@ import java.util.function.Consumer;
 public class FormLogoutSuccessEventListener implements Consumer<HttpSessionEvent> {
     @Override
     public void accept(HttpSessionEvent event) {
+        SecurityContextHolder.getContext().getAuthentication()
         log.info("Logged out successfully for user: {}", event);
     }
 }
