fa4fffbc463659940d9c0a25e013421ce7f76755
louis <pm6422@126.com>
2023-10-11 20:01:31 +0800

    refactor

--- luix-passport-client/src/main/java/cn/luixtech/passport/client/web/HomeController.java
+++ luix-passport-client/src/main/java/cn/luixtech/passport/client/web/HomeController.java
@@ -1,6 +1,11 @@
 package cn.luixtech.passport.client.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +16,12 @@ public class HomeController {
     }
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof OidcUser) {
+            OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+            model.addAttribute("username", oidcUser.getName());
+        }
         return "index";
     }
 
