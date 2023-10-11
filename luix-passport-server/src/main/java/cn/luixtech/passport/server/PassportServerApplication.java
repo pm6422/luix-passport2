ea3aa110e8fe716af6bfd7efb29206da3469f9ee
louis <pm6422@126.com>
2023-10-11 10:22:35 +0800

    refactor

---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/PassportServerApplication.java
@@ -0,0 +1,13 @@
+package cn.luixtech.passport.server;
+
+import com.luixtech.springbootframework.EnableLuixSpringBootFramework;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableLuixSpringBootFramework
+public class PassportServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassportServerApplication.class, args);
+    }
+}
