cd32f781e798a2a2133753123b05c61f3512f524
louis <pm6422@126.com>
2023-10-11 11:04:16 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2020-2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.luixtech.passport.server.config;
 
 import cn.luixtech.passport.server.federation.FederatedIdentityAuthenticationSuccessHandler;
@@ -31,16 +16,11 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
-/**
- * @author Joe Grandja
- * @author Steve Riesenberg
- * @since 1.1
- */
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
-public class DefaultSecurityConfig {
+public class WebServerSecurityConfiguration {
 
-	// @formatter:off
+    // @formatter:off
 	@Bean
 	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http
@@ -63,11 +43,11 @@ public class DefaultSecurityConfig {
 	}
 	// @formatter:on
 
-	private AuthenticationSuccessHandler authenticationSuccessHandler() {
-		return new FederatedIdentityAuthenticationSuccessHandler();
-	}
+    private AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new FederatedIdentityAuthenticationSuccessHandler();
+    }
 
-	// @formatter:off
+    // @formatter:off
 	@Bean
 	public UserDetailsService users() {
 		UserDetails user = User.withDefaultPasswordEncoder()
@@ -79,14 +59,14 @@ public class DefaultSecurityConfig {
 	}
 	// @formatter:on
 
-	@Bean
-	public SessionRegistry sessionRegistry() {
-		return new SessionRegistryImpl();
-	}
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
-	@Bean
-	public HttpSessionEventPublisher httpSessionEventPublisher() {
-		return new HttpSessionEventPublisher();
-	}
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
 
 }
