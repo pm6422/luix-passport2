51206dddddb705290de9abeacc6aa65b12ff4a0b
louis <pm6422@126.com>
2023-10-11 18:09:52 +0800

    refactor

--- luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebClientConfiguration.java
+++ luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebClientConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2020-2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.luixtech.passport.client.config;
 
 import cn.luixtech.passport.client.authorization.DeviceCodeOAuth2AuthorizedClientProvider;
@@ -28,13 +13,8 @@ import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepo
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
-/**
- * @author Joe Grandja
- * @author Steve Riesenberg
- * @since 0.0.1
- */
 @Configuration
-public class WebClientConfig {
+public class WebClientConfiguration {
 
     @Bean
     public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
--- luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebServerSecurityConfiguration.java
+++ luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebServerSecurityConfiguration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2020-2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package cn.luixtech.passport.client.config;
 
 import org.springframework.context.annotation.Bean;
@@ -27,15 +12,9 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-/**
- * @author Joe Grandja
- * @author Dmitriy Dubson
- * @author Steve Riesenberg
- * @since 0.0.1
- */
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
-public class SecurityConfig {
+public class WebServerSecurityConfiguration {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
