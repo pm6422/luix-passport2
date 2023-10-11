3a965bfe7cdafaadb54326fb8ac19cf7c9534bd9
louis <pm6422@126.com>
2023-10-11 11:16:19 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
@@ -1,8 +1,7 @@
 package cn.luixtech.passport.server.config;
 
-import cn.luixtech.passport.server.config.oauth.handler.FormLoginSuccessHandler;
 import cn.luixtech.passport.server.config.oauth.handler.FederatedIdentityLoginSuccessHandler;
-
+import cn.luixtech.passport.server.config.oauth.handler.FormLoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @EnableWebSecurity
@@ -38,16 +36,12 @@ public class WebServerSecurityConfiguration {
 			.oauth2Login(oauth2Login ->
 				oauth2Login
 					.loginPage("/login")
-					.successHandler(authenticationSuccessHandler())
+					.successHandler(new FederatedIdentityLoginSuccessHandler())
 			);
 
 		return http.build();
 	}
 	// @formatter:on
-
-    private AuthenticationSuccessHandler authenticationSuccessHandler() {
-        return new FederatedIdentityLoginSuccessHandler();
-    }
 
     // @formatter:off
 	@Bean
