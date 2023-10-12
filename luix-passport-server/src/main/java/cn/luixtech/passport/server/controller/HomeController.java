2b6e183f956704b9c61dc5dd06a64515859def4b
louis <pm6422@126.com>
2023-10-12 16:36:35 +0800

    refactor

---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/HomeController.java
@@ -0,0 +1,24 @@
+package cn.luixtech.passport.server.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        // todo: refactor
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            User user = (User) authentication.getPrincipal();
+            model.addAttribute("username", user.getUsername());
+        }
+        return "index";
+    }
+}
