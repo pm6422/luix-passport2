91c209cdbaeb30e20f61d758f6314a488d7e333a
louis <pm6422@126.com>
2023-10-13 09:19:25 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
@@ -3,13 +3,11 @@ package cn.luixtech.passport.server.config;
 import cn.luixtech.passport.server.config.formauth.event.FormLoginSuccessEventListener;
 import cn.luixtech.passport.server.config.formauth.event.FormLogoutSuccessEventListener;
 import cn.luixtech.passport.server.config.formauth.event.LogoutHttpSessionEventPublisher;
-import cn.luixtech.passport.server.config.formauth.handler.FormLogoutSuccessHandler;
-import cn.luixtech.passport.server.config.oauth.handler.FederatedIdentityLoginSuccessHandler;
 import cn.luixtech.passport.server.config.formauth.handler.FormLoginSuccessHandler;
+import cn.luixtech.passport.server.config.oauth.handler.FederatedIdentityLoginSuccessHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
@@ -20,7 +18,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @AllArgsConstructor
