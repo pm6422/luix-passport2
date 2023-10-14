dda84c99947aa5d91da8bb681d6ce88a08d3f8f6
pm6422 <pm6422@126.com>
2023-10-14 20:34:05 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/event/FederatedIdentityLoginSuccessEventListener.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/event/FederatedIdentityLoginSuccessEventListener.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.function.Consumer;
-
 @Slf4j
 public class FederatedIdentityLoginSuccessEventListener implements Consumer<OAuth2User> {
     @Override
