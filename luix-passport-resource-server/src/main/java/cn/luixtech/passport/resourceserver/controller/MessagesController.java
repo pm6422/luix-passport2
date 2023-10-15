12e52a3e2fb1678f0d7ad558a807b69381b4a0b7
pm6422 <pm6422@126.com>
2023-10-15 21:09:55 +0800

    refactor

--- luix-passport-resource-server/src/main/java/cn/luixtech/passport/resourceserver/controller/MessagesController.java
+++ luix-passport-resource-server/src/main/java/cn/luixtech/passport/resourceserver/controller/MessagesController.java
@@ -1,4 +1,4 @@
-package cn.luixtech.passport.resourceserver.web;
+package cn.luixtech.passport.resourceserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
