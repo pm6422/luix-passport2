151b6f77319732b80419b86404015eb44434ae2c
louis <pm6422@126.com>
2023-10-16 10:52:12 +0800

    refactor

--- luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebServerSecurityConfiguration.java
+++ luix-passport-client/src/main/java/cn/luixtech/passport/client/config/WebServerSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package cn.luixtech.passport.client.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,11 +14,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
+@AllArgsConstructor
 public class WebServerSecurityConfiguration {
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     // @formatter:off
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests(authorize ->
 				authorize
@@ -30,12 +33,12 @@ public class WebServerSecurityConfiguration {
 			// See {@link org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI}
 			.oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/messaging-client-oidc"))
 			.oauth2Client(withDefaults())
-			.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)));
+			.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()));
 		return http.build();
 	}
 	// @formatter:on
 
-    private LogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
         OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
 
         // Set the location that the End-User's User Agent will be redirected to
