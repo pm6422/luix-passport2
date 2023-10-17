5a4a9e6407e84b5313ee70b6dec78880f578dcfb
louis <pm6422@126.com>
2023-10-17 09:53:55 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebServerSecurityConfiguration.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * Use org.springframework.security.crypto.password.DelegatingPasswordEncoder as default
  */
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
 @Configuration(proxyBeanMethods = false)
 public class WebServerSecurityConfiguration {
