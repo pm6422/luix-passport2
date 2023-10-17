067fef6ad4ed97759c2802f8d97674ec89ff56aa
louis <pm6422@126.com>
2023-10-17 11:48:15 +0800

    add dynamic auth

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/UserAuthorityController.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/UserAuthorityController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import static cn.luixtech.passport.server.service.AuthorityService.*;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@PreAuthorize("hasAuthority(\"" + USER + "\")")
 public class UserAuthorityController {
     @Operation(summary = "find all authority list")
     @GetMapping("/api/user-authorities/all")
