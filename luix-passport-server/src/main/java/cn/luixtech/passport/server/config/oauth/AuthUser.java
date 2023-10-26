9f2f5f856a4981c5fee657bebe19203bbaaf2bba
louis <pm6422@126.com>
2023-10-26 09:31:00 +0800

    refactor

---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/oauth/AuthUser.java
@@ -0,0 +1,27 @@
+package cn.luixtech.passport.server.config.oauth;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class AuthUser extends User {
+    private static final long   serialVersionUID = -8021915441738843058L;
+    @Getter
+    private              String id;
+    @Getter
+    private              String firstName;
+    @Getter
+    private              String lastName;
+
+    public AuthUser(String id, String username, String firstName, String lastName,
+                    String password, boolean enabled,
+                    boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked,
+                    Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
