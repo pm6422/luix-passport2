39affcf0bfdbe9208e9d85bfc44b0c2136110c62
louis <pm6422@126.com>
2023-10-27 12:11:56 +0800

    refactor

---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/DatabaseConfiguration.java
@@ -0,0 +1,9 @@
+package cn.luixtech.passport.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfiguration {
+}
