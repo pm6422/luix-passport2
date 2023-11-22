9773050f4ae9baab6cc62523d0a199ae7f644e93
louis <pm6422@126.com>
2023-11-22 11:02:44 +0800

    refactor

--- docker/init-letsencrypt.sh
+++ docker/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(passport.luixtech.cn) # Change to your version, multiple domains are seperated by space key
+domains=(luixtech.cn passport.luixtech.cn) # Change to your version, multiple domains are seperated by space key
 rsa_key_size=4096
 
 # certbot output directory
