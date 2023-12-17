67ea4a345532f54f5b521a08640bf4c010bf2804
pm6422 <pm6422@126.com>
2023-12-17 13:40:24 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/security/CsrfRequireMatcher.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/security/CsrfRequireMatcher.java
@@ -5,9 +5,12 @@ import com.google.common.collect.ImmutableList;
 import com.luixtech.utilities.encryption.JasyptEncryptUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -31,11 +34,17 @@ public class CsrfRequireMatcher implements RequestMatcher {
         final String referer = request.getHeader("Referer");
         log.info("Referer: {}", referer);
         if (referer != null && IGNORED_PATHS.stream().anyMatch(referer::contains)) {
-            String query = URI.create(referer).getQuery();
-            String tokenValue = query.substring(query.indexOf("token=") + 6);
-            String decryptedValue = JasyptEncryptUtils.decrypt(tokenValue);
-            if (AuthUtils.getCurrentUsername().equals(decryptedValue)) {
-                return false;
+            MultiValueMap<String, String> parameters = UriComponentsBuilder.fromUriString(referer).build().getQueryParams();
+            if (MapUtils.isNotEmpty(parameters) && StringUtils.isNotEmpty(parameters.getFirst("token"))) {
+                String tokenValue = parameters.getFirst("token");
+                try {
+                    String decryptedValue = JasyptEncryptUtils.decrypt(tokenValue);
+                    if (AuthUtils.getCurrentUsername().equals(decryptedValue)) {
+                        return false;
+                    }
+                } catch (Exception ex) {
+                    // ignore
+                }
             }
         }
         // otherwise, CSRF is required
