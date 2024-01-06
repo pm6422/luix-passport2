e14c6ab2d02e33ae18cdeeadbeae8749cf01e7ae
pm6422 <pm6422@126.com>
2024-01-06 14:10:02 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/TestController.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/TestController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 import static cn.luixtech.passport.server.config.AuthorizationServerConfiguration.DEFAULT_PASSWORD_ENCODER_PREFIX;
 import static cn.luixtech.passport.server.service.impl.UserServiceImpl.BCRYPT_PASSWORD_ENCODER;
 
