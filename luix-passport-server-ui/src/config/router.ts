0e13e8e6f9efa8212d232be24cafff2fcac066a8
louis <pm6422@126.com>
2024-01-09 10:08:32 +0800

    add ui

--- luix-passport-server-ui/src/config/router.ts
+++ luix-passport-server-ui/src/config/router.ts
@@ -1,6 +1,6 @@
 import {
   createRouter,
-  createWebHashHistory,
+  createWebHistory,
   type RouteRecordRaw,
 } from "vue-router";
 import { useAuthStore } from "@/stores/auth-store";
@@ -219,7 +219,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
---
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/HomeController.java
@@ -0,0 +1,17 @@
+package cn.luixtech.passport.server.controller;
+
+import cn.luixtech.passport.server.utils.AuthUtils;
+import com.luixtech.utilities.encryption.JasyptEncryptUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static cn.luixtech.passport.server.service.AuthorityService.AUTH_DEVELOPER;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        return "/#/";
+    }
+}
