7ca3b99616b8288ccfb1576c455972ecace048cd
louis <pm6422@126.com>
2024-01-09 18:45:39 +0800

    add ui

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/HomeController.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/controller/HomeController.java
@@ -1,17 +1,12 @@
 package cn.luixtech.passport.server.controller;
 
-import cn.luixtech.passport.server.utils.AuthUtils;
-import com.luixtech.utilities.encryption.JasyptEncryptUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static cn.luixtech.passport.server.service.AuthorityService.AUTH_DEVELOPER;
 
 @Controller
 public class HomeController {
-//    @RequestMapping(value = {"/", "/index"})
-//    public String index(Model model) {
-//        return "index.html";
-//    }
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
