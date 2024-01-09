3cd75f6b9e12da2d9180679265bc52146d55d88d
pm6422 <pm6422@126.com>
2024-01-09 21:48:02 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebConfigurer.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/WebConfigurer.java
@@ -3,19 +3,10 @@ package cn.luixtech.passport.server.config;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.server.WebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-
-import static java.net.URLDecoder.decode;
 
 /**
  * Web application configuration
@@ -23,43 +14,8 @@ import static java.net.URLDecoder.decode;
 @Configuration
 @AllArgsConstructor
 @Slf4j
-public class WebConfigurer implements WebMvcConfigurer, WebServerFactoryCustomizer<WebServerFactory> {
+public class WebConfigurer implements WebMvcConfigurer {
     private final ApplicationProperties applicationProperties;
-
-    /**
-     * Customize the Servlet engine: Mime types, the document root, the cache.
-     */
-    @Override
-    public void customize(WebServerFactory server) {
-        // When running in an IDE or with ./mvnw spring-boot:run, set location of the static web assets.
-//        setLocationForStaticAssets(server);
-    }
-
-    private void setLocationForStaticAssets(WebServerFactory server) {
-        if (server instanceof ConfigurableServletWebServerFactory) {
-            ConfigurableServletWebServerFactory servletWebServer = (ConfigurableServletWebServerFactory) server;
-            File root;
-            String prefixPath = resolvePathPrefix();
-            root = new File(prefixPath + "target/classes/static/");
-            if (root.exists() && root.isDirectory()) {
-                servletWebServer.setDocumentRoot(root);
-            }
-        }
-    }
-
-    /**
-     * Resolve path prefix to static resources.
-     */
-    private String resolvePathPrefix() {
-        String fullExecutablePath = decode(this.getClass().getResource("").getPath(), StandardCharsets.UTF_8);
-        String rootPath = Paths.get(".").toUri().normalize().getPath();
-        String extractedPath = fullExecutablePath.replace(rootPath, "");
-        int extractionEndIndex = extractedPath.indexOf("target/");
-        if (extractionEndIndex <= 0) {
-            return "";
-        }
-        return extractedPath.substring(0, extractionEndIndex);
-    }
 
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
