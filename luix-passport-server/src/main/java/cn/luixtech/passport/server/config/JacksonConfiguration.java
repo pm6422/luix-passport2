9aa341870f7b7b016696c5d1da61d879bfa8f15f
louis <pm6422@126.com>
2024-01-11 16:18:50 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/config/JacksonConfiguration.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/config/JacksonConfiguration.java
@@ -22,24 +22,4 @@ public class JacksonConfiguration {
     public Jdk8Module jdk8TimeModule() {
         return new Jdk8Module();
     }
-
-    /**
-     * Module for serialization/deserialization of RFC7807 Problem.
-     *
-     * @return module instance
-     */
-//    @Bean
-//    public ProblemModule problemModule() {
-//        return new ProblemModule();
-//    }
-
-    /**
-     * Module for serialization/deserialization of ConstraintViolationProblem.
-     *
-     * @return module instance
-     */
-//    @Bean
-//    public ConstraintViolationProblemModule constraintViolationProblemModule() {
-//        return new ConstraintViolationProblemModule();
-//    }
 }
