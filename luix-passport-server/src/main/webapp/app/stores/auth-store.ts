436e7354bb06ccab70e9a8d880ab8b4b09911c71
louis <pm6422@126.com>
2024-01-12 11:33:31 +0800

    refactor

--- luix-passport-server/src/main/webapp/app/stores/auth-store.ts
+++ luix-passport-server/src/main/webapp/app/stores/auth-store.ts
@@ -4,9 +4,9 @@ import ApiService from "@/services/api-service";
 import type { IAuthUser } from '@/domain/AuthUser';
 
 export const useAuthStore = defineStore("auth", () => {
-  const errors = ref({});
   const user = ref<IAuthUser>({} as IAuthUser);
   const isAuthenticated = ref(false);
+  const errors = ref({});
 
   function verifyAuth() {
     return ApiService.query("open-api/accounts/user", {})
@@ -15,24 +15,24 @@ export const useAuthStore = defineStore("auth", () => {
       })
       .catch(({ response }) => {
         setError(response.data.errors);
-        purgeAuth();
+        deleteAuth();
       });
   }
 
-  function login(credentials: IAuthUser) {
-    return ApiService.post("login", credentials)
-      .then(({ data }) => {
-        setAuth(data);
-      })
-      .catch(({ response }) => {
-        setError(response.data.errors);
-      });
-  }
+  // function login(credentials: IAuthUser) {
+  //   return ApiService.post("login", credentials)
+  //     .then(({ data }) => {
+  //       setAuth(data);
+  //     })
+  //     .catch(({ response }) => {
+  //       setError(response.data.errors);
+  //     });
+  // }
 
   function logout() {
     ApiService.post("logout", {})
       .then(({ data }) => {
-        purgeAuth();
+        deleteAuth();
         window.location.reload();
       })
       .catch(({ response }) => {
@@ -40,25 +40,25 @@ export const useAuthStore = defineStore("auth", () => {
       });
   }
 
-  function register(credentials: IAuthUser) {
-    return ApiService.post("register", credentials)
-      .then(({ data }) => {
-        setAuth(data);
-      })
-      .catch(({ response }) => {
-        setError(response.data.errors);
-      });
-  }
+  // function register(credentials: IAuthUser) {
+  //   return ApiService.post("register", credentials)
+  //     .then(({ data }) => {
+  //       setAuth(data);
+  //     })
+  //     .catch(({ response }) => {
+  //       setError(response.data.errors);
+  //     });
+  // }
 
-  function forgotPassword(email: string) {
-    return ApiService.post("forgot_password", email)
-      .then(() => {
-        setError({});
-      })
-      .catch(({ response }) => {
-        setError(response.data.errors);
-      });
-  }
+  // function forgotPassword(email: string) {
+  //   return ApiService.post("forgot_password", email)
+  //     .then(() => {
+  //       setError({});
+  //     })
+  //     .catch(({ response }) => {
+  //       setError(response.data.errors);
+  //     });
+  // }
 
   function setAuth(authUser: IAuthUser) {
     if(authUser.username === 'anonymousUser') {
@@ -69,24 +69,21 @@ export const useAuthStore = defineStore("auth", () => {
     errors.value = {};
   }
 
-  function setError(error: any) {
-    errors.value = { ...error };
-  }
-
-  function purgeAuth() {
+  function deleteAuth() {
     isAuthenticated.value = false;
     user.value = {} as IAuthUser;
     errors.value = [];
+  }
+
+  function setError(error: any) {
+    errors.value = { ...error };
   }
 
   return {
     errors,
     user,
     isAuthenticated,
-    login,
     logout,
-    register,
-    forgotPassword,
     verifyAuth,
   };
 });
