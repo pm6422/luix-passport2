ad8b2acd5454d75b0d4ee6239b03242d30f923af
pm6422 <pm6422@126.com>
2024-03-04 22:38:06 +0800

    refactor

--- luix-passport-server/src/main/java/cn/luixtech/passport/server/PassportServerApplication.java
+++ luix-passport-server/src/main/java/cn/luixtech/passport/server/PassportServerApplication.java
@@ -1,10 +1,8 @@
 package cn.luixtech.passport.server;
 
 import com.luixtech.springbootframework.EnableLuixSpringBootFramework;
-import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
