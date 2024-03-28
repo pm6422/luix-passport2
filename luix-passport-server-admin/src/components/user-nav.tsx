29ac876c0be075d4089d8460d5607becbc82ab67
louis <pm6422@126.com>
2024-03-28 17:39:34 +0800

    r

--- luix-passport-server-admin/src/components/user-nav.tsx
+++ luix-passport-server-admin/src/components/user-nav.tsx
@@ -15,19 +15,19 @@ export function UserNav() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-          <Avatar className='h-8 w-8'>
-            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-            <AvatarFallback>SN</AvatarFallback>
+        <Button variant='ghost' className='relative h-12 w-12 rounded-full'>
+          <Avatar className='h-12 w-12'>
+            <AvatarImage src='/images/avatar/louis.jpg' alt='@shadcn' />
+            <AvatarFallback>Louis</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>satnaing</p>
+            <p className='text-sm font-medium leading-none'>Louis Lau</p>
             <p className='text-xs leading-none text-muted-foreground'>
-              satnaingdev@gmail.com
+              louis@luixtech.cn
             </p>
           </div>
         </DropdownMenuLabel>
