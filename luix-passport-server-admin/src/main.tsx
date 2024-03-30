e131ff1b9090c57ea79e0a6cfd631125d33d99bc
pm6422 <pm6422@126.com>
2024-03-30 17:22:58 +0800

    r

--- luix-passport-server-admin/src/main.tsx
+++ luix-passport-server-admin/src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/custom/theme-provider'
 import router from '@/router'
-import '@/index.css'
+import '@/assets/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
