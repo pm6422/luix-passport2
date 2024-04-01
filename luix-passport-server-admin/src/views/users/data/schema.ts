389fbaf43a9555094b7ef9806e588a5dbcaafb61
louis <pm6422@126.com>
2024-04-01 19:22:32 +0800

    r

--- luix-passport-server-admin/src/views/users/data/schema.ts
+++ luix-passport-server-admin/src/views/users/data/schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 // We're keeping a simple non-relational schema here.
 // IRL, you will have a schema for your data models.
-export const schema = z.object({
+export const taskSchema = z.object({
   id: z.string(),
   title: z.string(),
   status: z.string(),
@@ -10,4 +10,4 @@ export const schema = z.object({
   priority: z.string(),
 })
 
-export type Task = z.infer<typeof schema>
+export type Task = z.infer<typeof taskSchema>
