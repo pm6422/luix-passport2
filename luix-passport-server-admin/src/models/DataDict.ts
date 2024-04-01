3e9bb4f230fc38715d507e62eae05a44991e504f
louis <pm6422@126.com>
2024-04-01 17:14:46 +0800

    refactor

---
+++ luix-passport-server-admin/src/models/DataDict.ts
@@ -0,0 +1,10 @@
+export interface IDataDict {
+    id: string;
+    num: string;
+    categoryCode: string;
+    dictCode: string;
+    dictName?: string;
+    remark?: string;
+    enabled: boolean;
+    updatedTime?: string;
+  }
