c0a1e8136c13081a94629549a333e00b8171f151
louis <pm6422@126.com>
2024-04-01 18:43:13 +0800

    r

--- luix-passport-server-admin/src/views/users/uers.tsx
+++ luix-passport-server-admin/src/views/users/uers.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { AccountNav } from '@/components/account-nav.tsx'
 import { Layout, LayoutBody, LayoutHeader } from '@/layouts/layout-definitions'
 import { DataTable } from './components/data-table'
@@ -6,10 +7,26 @@ import { tasks } from './data/tasks'
 import { DataDictService } from '@/services/data-dict-service'
 
 export default function Tasks() {
-  
-  DataDictService.findAll().then(results => {
-    
-  })
+  // State to hold the fetched data
+  const [dataDicts, setDataDicts] = useState(null);
+
+  useEffect(() => {
+    // Function to fetch data from backend
+    const fetchData = async () => {
+      // Promise.all([DataDictService.findAll(), UserService.findAll()])
+      // .then(function (results) {
+      //   const dicts = results[0];
+      //   const users = results[1];
+      // });
+      const response = await DataDictService.findAll();
+      setDataDicts(response.data);
+    };
+
+    // Call the fetchData function when component mounts
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array ensures useEffect runs only once on component mount
 
 
   return (
