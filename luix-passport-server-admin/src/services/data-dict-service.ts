cfc4781610600c5defa9b1fdae521c733aef149c
louis <pm6422@126.com>
2024-04-01 17:22:39 +0800

    r

--- luix-passport-server-admin/src/services/data-dict-service.ts
+++ luix-passport-server-admin/src/services/data-dict-service.ts
@@ -1,39 +1,39 @@
 import axios from 'axios'
-import ApiService from "@/services/api-service"
-import type { IDataDict } from '@/domain/DataDict'
+import type { IDataDict } from '@/models/DataDict'
+
 export class DataDictService {
   constructor() {
   }
 
   public static find(reqParams: any): Promise<any> {
-    return ApiService.query("api/data-dicts", { params: reqParams });
+    return axios.get("api/data-dicts", { params: reqParams });
   }
 
   public static findAll(enabled: boolean | null = null): Promise<any> {
-    return ApiService.query("api/data-dicts", { params: { page: 0, size: 2000, enabled: enabled } });
+    return axios.get("api/data-dicts", { params: { page: 0, size: 2000, enabled: enabled } });
   }
 
   public static lookup(categoryCode: string, enabled: boolean | null = null): Promise<any> {
-    return ApiService.query("api/data-dicts", { params: { page: 0, size: 2000, categoryCode: categoryCode, enabled: enabled } });
+    return axios.get("api/data-dicts", { params: { page: 0, size: 2000, categoryCode: categoryCode, enabled: enabled } });
   }
 
   public static findById(id: String): Promise<any> {
-    return ApiService.query("api/data-dicts/" + id, null);
+    return axios.get("api/data-dicts/" + id);
   }
 
   public static create(model: IDataDict): Promise<any> {
-    return ApiService.post("api/data-dicts", model);
+    return axios.post("api/data-dicts", model);
   }
 
   public static update(model: IDataDict): Promise<any> {
-    return ApiService.put("api/data-dicts", model);
+    return axios.put("api/data-dicts", model);
   }
 
   public static deleteById(id: String): Promise<any> {
-    return ApiService.delete("api/data-dicts/" + id);
+    return axios.delete("api/data-dicts/" + id);
   }
 
   public static batchUpdate(ids: Array<string>, targetCategoryCode: string): Promise<any> {
-    return ApiService.put("api/data-dicts/batch-update", { ids: ids, targetCategoryCode: targetCategoryCode });
+    return axios.put("api/data-dicts/batch-update", { ids: ids, targetCategoryCode: targetCategoryCode });
   }
 }
