d11dcde7c684fa703b64ae51885d841ab2c585c5
louis <pm6422@126.com>
2024-04-02 11:27:11 +0800

    r

--- luix-passport-server-admin/src/utils/utils.ts
+++ luix-passport-server-admin/src/utils/utils.ts
@@ -2,16 +2,14 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import dayjs from 'dayjs'
 
-export const defaultTimeZone = "Asia/Shanghai";
 export const defaultDateTimeFormat = "YYYY-MM-DD HH:mm:ss";
+export const defaultDateFormat = "YYYY-MM-DD HH:mm:ss";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-/**
- * @description format date time with the specified format
- */
+
 export function formatDateTime(value: string): string {
   if (!value) {
     return '';
@@ -19,12 +17,9 @@ export function formatDateTime(value: string): string {
   return dayjs(value).format(defaultDateTimeFormat);
 }
 
-/**
- * @description format date 
- */
-export function formatDate(value: string, format: string): string {
+export function formatDate(value: string): string {
   if (!value) {
     return '';
   }
-  return dayjs(value).format(format);
+  return dayjs(value).format(defaultDateFormat);
 }
