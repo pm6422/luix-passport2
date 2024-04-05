f0bc60b2983e38baac1bd4476eaee8e4853fe869
pm6422 <pm6422@126.com>
2024-04-05 23:02:12 +0800

    r

--- luix-passport-server-admin/src/views/data-dicts/table/table-toolbar.tsx
+++ luix-passport-server-admin/src/views/data-dicts/table/table-toolbar.tsx
@@ -21,39 +21,41 @@ export function DataTableToolbar ({
   return (
     <div className='flex items-center justify-between w-full'>
       <div className='flex flex-col sm:flex-row sm:items-center sm:space-x-2'>
-        <div className='flex flex-wrap w-full sm:w-auto sm:space-x-2'>
+        <div className='flex flex-wrap w-full sm:w-auto space-x-2'>
           <Input
             placeholder='Number'
             value={criteria.num}
             onChange={(event) => setCriteria({ ...criteria, num: event.target.value })}
-            className='h-8 w-[90px] lg:w-[130px]'
+            className='h-8 w-[130px] lg:w-[130px]'
           />
           <Input
             placeholder='Category Code'
             value={criteria.categoryCode}
             onChange={(event) => setCriteria({ ...criteria, categoryCode: event.target.value })}
-            className='h-8 w-[90px] lg:w-[130px]'
+            className='h-8 w-[130px] lg:w-[130px] mb-2 sm:mb-0' // Add mb-2 for spacing between fields
           />
+        </div>
+        <div className='flex flex-wrap w-full sm:w-auto space-x-2'>
           <EnabledSelect
             value={criteria.enabled}
             onValueChange={(value) => setCriteria({ ...criteria, enabled: value })}
-            className='h-8 w-[90px] lg:w-[130px] mb-2 sm:mb-0' // Add mb-2 for spacing between fields
+            className='h-8 w-[130px] lg:w-[130px]'
           />
+          <Button
+            variant='link'
+            onClick={() => setCriteria(initialCriteria)}
+            className='h-8 lg:px-3 lg:flex hidden' // Hide on small screens
+          >
+            <IconX className='h-4 w-4' />
+          </Button>
+          <Button
+            onClick={() => loadPage()}
+            className='h-8 px-2 lg:px-3'
+          >
+            <IconSearch className='mr-2 h-4 w-4' />
+            Search
+          </Button>
         </div>
-        <Button
-          variant='link'
-          onClick={() => setCriteria(initialCriteria)}
-          className='h-8 lg:px-3 lg:flex hidden' // Hide on small screens
-        >
-          <IconX className='h-4 w-4' />
-        </Button>
-        <Button
-          onClick={() => loadPage()}
-          className='h-8 px-2 lg:px-3'
-        >
-          <IconSearch className='mr-2 h-4 w-4' />
-          Search
-        </Button>
       </div>
       <Button
         onClick={() => create()}
