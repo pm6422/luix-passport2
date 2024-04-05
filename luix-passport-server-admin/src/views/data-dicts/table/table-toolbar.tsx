fe829661b474d319f8d6d524a1978afc953a1303
pm6422 <pm6422@126.com>
2024-04-05 22:43:09 +0800

    r

--- luix-passport-server-admin/src/views/data-dicts/table/table-toolbar.tsx
+++ luix-passport-server-admin/src/views/data-dicts/table/table-toolbar.tsx
@@ -20,28 +20,30 @@ export function DataTableToolbar ({
 }: DataTableToolbarProps) {
   return (
     <div className='flex items-center justify-between w-full'>
-      <div className='flex flex-1 flex-col items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2'>
-        <Input
-          placeholder='Number'
-          value={criteria.num}
-          onChange={(event) => setCriteria({ ...criteria, num: event.target.value })}
-          className='h-8 w-[90px] lg:w-[130px]'
-        />
-        <Input
-          placeholder='Category Code'
-          value={criteria.categoryCode}
-          onChange={(event) => setCriteria({ ...criteria, categoryCode: event.target.value })}
-          className='h-8 w-[90px] lg:w-[130px]'
-        />
-        <EnabledSelect
-          value={criteria.enabled}
-          onValueChange={(value) => setCriteria({ ...criteria, enabled: value })}
-          className='h-8 w-[90px] lg:w-[130px]'
-        />
+      <div className='flex flex-col sm:flex-row sm:items-center sm:space-x-2'>
+        <div className='flex flex-wrap w-full sm:w-auto sm:space-x-2'>
+          <Input
+            placeholder='Number'
+            value={criteria.num}
+            onChange={(event) => setCriteria({ ...criteria, num: event.target.value })}
+            className='h-8 w-[90px] lg:w-[130px] mb-2 sm:mb-0' // Add mb-2 for spacing between fields
+          />
+          <Input
+            placeholder='Category Code'
+            value={criteria.categoryCode}
+            onChange={(event) => setCriteria({ ...criteria, categoryCode: event.target.value })}
+            className='h-8 w-[90px] lg:w-[130px]' // No bottom margin for the last field
+          />
+          <EnabledSelect
+            value={criteria.enabled}
+            onValueChange={(value) => setCriteria({ ...criteria, enabled: value })}
+            className='h-8 w-[90px] lg:w-[130px] mb-2 sm:mb-0' // Add mb-2 for spacing between fields
+          />
+        </div>
         <Button
           variant='secondary'
           onClick={() => setCriteria(initialCriteria)}
-          className='h-8 px-2 lg:px-3 lg:flex hidden'
+          className='h-8 px-2 lg:px-3 lg:flex hidden' // Hide on small screens
         >
           <IconX className='mr-2 h-4 w-4' />
           Reset
