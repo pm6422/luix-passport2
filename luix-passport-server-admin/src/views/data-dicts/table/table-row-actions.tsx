7a92ed4423ca433439226d5501afb039a2bd5e2f
louis <pm6422@126.com>
2024-04-16 16:14:51 +0800

    r

--- luix-passport-server-admin/src/views/data-dicts/table/table-row-actions.tsx
+++ luix-passport-server-admin/src/views/data-dicts/table/table-row-actions.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { IconDots } from '@tabler/icons-react'
 import { Row } from '@tanstack/react-table'
 import { Button } from '@/components/custom/button'
@@ -8,12 +9,18 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 // import { tableSchema } from './table-schema'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>
 }
 
 export function DataTableRowActions<TData>({row}: DataTableRowActionsProps<TData>) {
+  const [delConfirmPopoverOpen, setDelConfirmPopoverOpen] = useState(false)
   // const rowData = tableSchema.parse(row.original)
   // console.log(row.original);
 
@@ -28,9 +35,40 @@ export function DataTableRowActions<TData>({row}: DataTableRowActionsProps<TData
           <span className='sr-only'>Open menu</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end' className='w-[160px]'>
-        <DropdownMenuItem>Edit</DropdownMenuItem>
-        <DropdownMenuItem>Delete</DropdownMenuItem>
+      <DropdownMenuContent align='end' className='w-[75px]'>
+        <Button variant="outline" className='w-full'>Edit</Button>
+        <Popover open={delConfirmPopoverOpen} onOpenChange={setDelConfirmPopoverOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className='w-full'>Delete</Button>
+          </PopoverTrigger>
+          <PopoverContent className='w-[240px]'>
+            Are your sure to delete it?
+            <div className="flex items-center justify-between mt-4 space-x-2">
+              <Button
+                className="w-full px-2"
+                variant="secondary"
+                size="sm"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  
+                }}
+              >
+                Yes
+              </Button>
+              <Button
+                className="w-full px-2"
+                variant="secondary"
+                size="sm"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  setDelConfirmPopoverOpen(false)
+                }}
+              >
+                No
+              </Button>
+            </div>
+          </PopoverContent>
+        </Popover>
       </DropdownMenuContent>
     </DropdownMenu>
   )
