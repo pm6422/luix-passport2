e3819c8158c9dae68f3d9f079bdd561056bdc501
pm6422 <pm6422@126.com>
2024-04-27 15:34:13 +0800

    r

--- luix-passport-server-admin/src/views/account-settings/profile/profile-form.tsx
+++ luix-passport-server-admin/src/views/account-settings/profile/profile-form.tsx
@@ -1,21 +1,10 @@
-import { useForm, useFieldArray } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { Button } from "@/components/custom/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
-import { RequiredFormLabel } from "@/components/custom/required-form-label"
+import { Form } from "@/components/ui/form"
+import InputFormField from "@/components/custom/form/input"
 import { toast } from "sonner"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
-import { cn } from "@/libs/utils"
 
 const profileFormSchema = z.object({
   username: z
@@ -41,11 +30,7 @@ type ProfileFormValues = z.infer<typeof profileFormSchema>
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
-  bio: "I own a computer.",
-  urls: [
-    { value: "https://shadcn.com" },
-    { value: "http://twitter.com/shadcn" },
-  ],
+  bio: "I own a computer."
 }
 
 export default function ProfileForm() {
@@ -53,11 +38,6 @@ export default function ProfileForm() {
     resolver: zodResolver(profileFormSchema),
     defaultValues,
     mode: "onChange",
-  })
-
-  const { fields, append } = useFieldArray({
-    name: "urls",
-    control: form.control,
   })
 
   function onSubmit(data: ProfileFormValues) {
@@ -74,89 +54,17 @@ export default function ProfileForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <RequiredFormLabel>Username</RequiredFormLabel>
-              <FormControl>
-                <Input placeholder="louis" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name. It can be your real name or a
-                pseudonym. You can only change this once every 30 days.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+        <InputFormField control={form.control} name="username" label="Username" required 
+          description="This is your public display name. It can be your real name or a
+          pseudonym. You can only change this once every 30 days."
         />
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <RequiredFormLabel>Email</RequiredFormLabel>
-              <FormControl>
-                <Input {...field}/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
+        <InputFormField control={form.control} name="email" label="Email" required/>
+
+        <InputFormField control={form.control} name="bio" label="Bio" type="textarea"
+          description="You can @mention other users and organizations to
+          link to them."
         />
-        <FormField
-          control={form.control}
-          name="bio"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Bio</FormLabel>
-              <FormControl>
-                <Textarea
-                  placeholder="Tell us a little bit about yourself"
-                  className="resize-none"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                You can <span>@mention</span> other users and organizations to
-                link to them.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div>
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`urls.${index}.value`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className={cn(index !== 0 && "sr-only")}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && "sr-only")}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          ))}
-          <Button
-            type="button"
-            variant="outline"
-            size="sm"
-            className="mt-2"
-            onClick={() => append({ value: "" })}
-          >
-            Add URL
-          </Button>
-        </div>
+
         <Button type="submit">Update profile</Button>
       </form>
     </Form>
