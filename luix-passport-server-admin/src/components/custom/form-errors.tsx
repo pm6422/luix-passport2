55abcd0208e024a44ea9e82ae395dd445e63481e
louis <pm6422@126.com>
2024-04-28 14:11:48 +0800

    r

--- luix-passport-server-admin/src/components/custom/form-errors.tsx
+++ luix-passport-server-admin/src/components/custom/form-errors.tsx
@@ -1,17 +1,22 @@
 import { UseFormReturn } from "react-hook-form"
 import { IconExclamationCircle } from "@tabler/icons-react"
-import { Alert, AlertTitle } from "@/components/ui/alert"
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert"
 
 interface Props {
+  children?: React.ReactNode;
   form: UseFormReturn<any, any, any>;
+  variant?: 'default' | 'destructive';
 }
 
 const FormErrors = ({
+  children,
   form,
+  variant = 'destructive'
 }: Props) => (
   Object.values(form.formState.errors).length > 0 && (
-    <Alert variant="destructive">
+    <Alert variant={variant}>
       <AlertTitle className="flex items-center"><IconExclamationCircle className="size-5 me-1" />Please check your input.</AlertTitle>
+      {children && <AlertDescription className="pr-8 font-light">{children}</AlertDescription>}
     </Alert>
   )
 );
