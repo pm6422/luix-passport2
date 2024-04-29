a210518e430d025b47472b3920b2d73473fe98dd
louis <pm6422@126.com>
2024-04-29 11:58:11 +0800

    r

---
+++ luix-passport-server-admin/src/axios.ts
@@ -0,0 +1,48 @@
+import axios from "axios"
+import qs from "qs"
+const { REACT_APP_SERVER_URL } = process.env
+
+const instance = axios.create({
+  baseURL: `${REACT_APP_SERVER_URL}`,
+  paramsSerializer(params) {
+    return qs.stringify(params, { indices: undefined })
+  },
+})
+
+instance.interceptors.request.use(
+  function (config) {
+    // Modify request config before sending
+    // For example, you can add headers or perform authentication checks
+    config.headers.Authorization = 'Bearer YourAccessToken'
+    return config
+  },
+  function (error) {
+    // Handle request errors
+    return Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use(
+  function (response) {
+    // Modify response data before resolving the promise
+    // For example, you can handle success messages or extract data
+    console.log('Response Data:', response.data)
+    return response
+  },
+  function (error) {
+    // Handle response errors
+    // For example, you can handle unauthorized errors or network failures
+    if (error.response.status === 401) {
+      // Handle unauthorized access
+      console.error('Unauthorized access')
+    } else {
+      // Handle other errors
+      console.error('Request failed:', error.message)
+    }
+    return Promise.reject(error)
+  }
+)
+
+export const http = instance
+
+export default http
