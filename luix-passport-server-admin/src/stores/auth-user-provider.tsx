cf364884141886b1c0d1a5a5d19c7ff5b742a8c5
louis <pm6422@126.com>
2024-04-29 14:55:10 +0800

    r

--- luix-passport-server-admin/src/stores/auth-user-provider.tsx
+++ luix-passport-server-admin/src/stores/auth-user-provider.tsx
@@ -2,34 +2,34 @@ import React, { createContext, useContext, useState, useEffect } from "react"
 import { isEmpty } from "lodash"
 
 export type AuthUser = {
-  username: string;
-  email: string;
-  roles: Array<string>;
-};
+  username: string
+  email: string
+  roles: Array<string>
+}
 
 type AuthUserProviderProps = {
-  children: React.ReactNode;
-  defaultValue?: AuthUser | null;
-};
+  children: React.ReactNode
+  defaultValue?: AuthUser | null
+}
 
 type AuthUserProviderState = {
-  authUser: AuthUser | null;
-  clearAuthUser: () => void;
-};
+  authUser: AuthUser | null
+  clearAuthUser: () => void
+}
 
 const initialState: AuthUserProviderState = {
   authUser: null,
   clearAuthUser: () => {},
-};
+}
 
-const AuthUserProviderContext = createContext<AuthUserProviderState>(initialState);
+const AuthUserProviderContext = createContext<AuthUserProviderState>(initialState)
 
 export function AuthUserProvider({
   children,
   defaultValue,
   ...props
 }: AuthUserProviderProps) {
-  const [authUser, setAuthUser] = useState<AuthUser | null>(defaultValue || null);
+  const [authUser, setAuthUser] = useState<AuthUser | null>(defaultValue || null)
 
   // watch auth user whether is null
   useEffect(() => {
@@ -41,21 +41,21 @@ export function AuthUserProvider({
   const value = {
     authUser,
     clearAuthUser: () => setAuthUser(null)
-  };
+  }
 
   return (
     <AuthUserProviderContext.Provider {...props} value={value}>
       {children}
     </AuthUserProviderContext.Provider>
-  );
+  )
 }
 
 export const useAuthUserProvider = () => {
-  const context = useContext(AuthUserProviderContext);
+  const context = useContext(AuthUserProviderContext)
 
   if (context === undefined) {
-    throw new Error("useAuthUserProvider must be used within a AuthUserProvider");
+    throw new Error("useAuthUserProvider must be used within a AuthUserProvider")
   }
 
-  return context;
-};
+  return context
+}
