57edf1931d65633892c3a54a5b40ecbf85ea3cdc
pm6422 <pm6422@126.com>
2024-04-30 23:05:07 +0800

    r

--- luix-passport-server/frontend/src/components/custom/avatar-uploader.tsx
+++ luix-passport-server/frontend/src/components/custom/avatar-uploader.tsx
@@ -1,13 +1,13 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/custom/button";
-import { Input } from "@/components/ui/input";
-import { toBase64 } from "@/libs/utils";
-import { IconPencil, IconUserScan } from "@tabler/icons-react";
-import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/custom/button"
+import { Input } from "@/components/ui/input"
+import { toBase64 } from "@/libs/utils"
+import { IconPencil, IconUserScan } from "@tabler/icons-react"
+import React from "react"
 
 type AvatarUploaderProps = {
-	defaultValue?: string;
-	onValueChange?: (value?: string) => void;
+	defaultValue?: string
+	onValueChange?: (value?: string) => void
 }
 
 export function AvatarUploader({
@@ -18,9 +18,9 @@ export function AvatarUploader({
 	const inputRef = React.useRef<HTMLInputElement>(null)
 	const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files && e.target.files.length > 0) {
-			const file = e.target.files[0];
-			const base64 = await toBase64(file) as string;
-			onValueChange?.(base64);
+			const file = e.target.files[0]
+			const base64 = await toBase64(file) as string
+			onValueChange?.(base64)
 		}
 	}
 	
