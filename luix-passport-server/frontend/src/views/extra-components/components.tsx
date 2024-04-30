e7a6512440406628d66f7762403dbca3b000ee3e
louis <pm6422@126.com>
2024-04-30 10:51:43 +0800

    r

--- luix-passport-server/frontend/src/views/extra-components/components.tsx
+++ luix-passport-server/frontend/src/views/extra-components/components.tsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom'
 import { IconChevronRight } from '@tabler/icons-react'
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { Layout, LayoutBody, LayoutHeader } from '@/layouts/layout-definitions'
+import { LayoutBody } from '@/layouts/layout-definitions'
 import { Breadcrumb, BreadcrumbItem } from '@/components/custom/breadcrumb'
 import { PinInput, PinInputField } from '@/components/custom/pin-input'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Separator } from '@/components/ui/separator'
 import { Input } from '@/components/ui/input'
-import { AccountNav } from '@/components/account-nav.tsx'
 import {
   Form,
   FormControl,
@@ -32,7 +31,6 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { languages } from "@/data/languages"
-import Combobox from '@/components/custom/combobox'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -80,203 +78,194 @@ export default function ExtraComponents() {
   }
 
   return (
-    <Layout>
-      {/* ===== Top Heading ===== */}
-      <LayoutHeader>
-        <div className='ml-auto flex items-center space-x-4'>
-          <AccountNav />
-        </div>
-      </LayoutHeader>
+    <LayoutBody className='space-y-4'>
+      <div className='flex items-center justify-between space-y-2'>
+        <h1 className='text-2xl font-bold tracking-tight md:text-3xl'>
+          Extra Components
+        </h1>
+      </div>
+      <h2 className='text-lg font-bold md:text-xl'>Breadcrumbs</h2>
+      <Breadcrumb separator={<IconChevronRight size={18} />}>
+        {items}
+      </Breadcrumb>
+      <Breadcrumb>{items}</Breadcrumb>
 
-      <LayoutBody className='space-y-4'>
-        <div className='flex items-center justify-between space-y-2'>
-          <h1 className='text-2xl font-bold tracking-tight md:text-3xl'>
-            Extra Components
-          </h1>
-        </div>
-        <h2 className='text-lg font-bold md:text-xl'>Breadcrumbs</h2>
-        <Breadcrumb separator={<IconChevronRight size={18} />}>
-          {items}
-        </Breadcrumb>
-        <Breadcrumb>{items}</Breadcrumb>
+      <Separator />
 
-        <Separator />
-
-        <h2 className='text-lg font-bold md:text-xl'>Pin Input</h2>
-        <div className='flex flex-col gap-12 lg:flex-row'>
-          <div className='flex-1'>
-            <h3 className='mb-2 font-medium'>Uncontrolled</h3>
-            <Tabs defaultValue='preview'>
-              <TabsList>
-                <TabsTrigger value='preview'>Preview</TabsTrigger>
-                <TabsTrigger value='code'>Code</TabsTrigger>
-              </TabsList>
-              <TabsContent value='preview'>
-                <div className='flex min-h-56 items-center justify-center rounded border'>
-                  <PinInput
-                    className='flex h-10 space-x-4'
-                    onComplete={(str) => console.log('completed', str)}
-                    autoFocus
-                  >
-                    <PinInputField component={Input} />
-                    <PinInputField component={Input} />
-                    <Separator orientation='vertical' />
-                    <PinInputField component={Input} />
-                    <PinInputField component={Input} />
-                  </PinInput>
-                </div>
-              </TabsContent>
-              <TabsContent value='code'>
-                <SyntaxHighlighter
-                  language='tsx'
-                  style={nord}
-                  wrapLines
-                  wrapLongLines
+      <h2 className='text-lg font-bold md:text-xl'>Pin Input</h2>
+      <div className='flex flex-col gap-12 lg:flex-row'>
+        <div className='flex-1'>
+          <h3 className='mb-2 font-medium'>Uncontrolled</h3>
+          <Tabs defaultValue='preview'>
+            <TabsList>
+              <TabsTrigger value='preview'>Preview</TabsTrigger>
+              <TabsTrigger value='code'>Code</TabsTrigger>
+            </TabsList>
+            <TabsContent value='preview'>
+              <div className='flex min-h-56 items-center justify-center rounded border'>
+                <PinInput
+                  className='flex h-10 space-x-4'
+                  onComplete={(str) => console.log('completed', str)}
+                  autoFocus
                 >
-                  {`<PinInput
-                    className='flex h-10 space-x-4'
-                    defaultValue=''
-                    onComplete={(str) => 
-                      console.log('completed', str)
-                    }  
-                    autoFocus
-                  >
-                    <PinInputField component={Input} />
-                    <PinInputField component={Input} />
-                    <Separator orientation='vertical' />
-                    <PinInputField component={Input} />
-                    <PinInputField component={Input} />
-                  </PinInput>
-                  `}
-                </SyntaxHighlighter>
-              </TabsContent>
-            </Tabs>
-          </div>
-          <div className='flex-1'>
-            <h3 className='mb-2 font-medium'>Controlled</h3>
-            <Tabs defaultValue='preview'>
-              <TabsList>
-                <TabsTrigger value='preview'>Preview</TabsTrigger>
-                <TabsTrigger value='code'>Code</TabsTrigger>
-              </TabsList>
-              <TabsContent value='preview'>
-                <div className='flex min-h-56 items-center justify-center rounded border'>
-                  <PinInput
-                    className='flex h-10 space-x-4'
-                    value={pinInput}
-                    onChange={setPinInput}
-                    onComplete={(str) => console.log('completed', str)}
-                  >
-                    {Array.from({ length: 4 }, (_, i) => (
-                      <PinInputField key={i} component={Input} />
-                    ))}
-                  </PinInput>
-                </div>
-              </TabsContent>
-              <TabsContent value='code'>
-                <SyntaxHighlighter
-                  language='tsx'
-                  style={nord}
-                  wrapLines
-                  wrapLongLines
+                  <PinInputField component={Input} />
+                  <PinInputField component={Input} />
+                  <Separator orientation='vertical' />
+                  <PinInputField component={Input} />
+                  <PinInputField component={Input} />
+                </PinInput>
+              </div>
+            </TabsContent>
+            <TabsContent value='code'>
+              <SyntaxHighlighter
+                language='tsx'
+                style={nord}
+                wrapLines
+                wrapLongLines
+              >
+                {`<PinInput
+                  className='flex h-10 space-x-4'
+                  defaultValue=''
+                  onComplete={(str) => 
+                    console.log('completed', str)
+                  }  
+                  autoFocus
                 >
-                  {`function ControlledPinInput() {
-                    const [pinInput, setPinInput] = useState('');
-                  
-                    return (
-                      <PinInput
-                        className='flex h-10 space-x-4'
-                        value={pinInput}
-                        onChange={setPinInput}
-                        onComplete={(str) => 
-                          console.log('completed', str)
-                        }
+                  <PinInputField component={Input} />
+                  <PinInputField component={Input} />
+                  <Separator orientation='vertical' />
+                  <PinInputField component={Input} />
+                  <PinInputField component={Input} />
+                </PinInput>
+                `}
+              </SyntaxHighlighter>
+            </TabsContent>
+          </Tabs>
+        </div>
+        <div className='flex-1'>
+          <h3 className='mb-2 font-medium'>Controlled</h3>
+          <Tabs defaultValue='preview'>
+            <TabsList>
+              <TabsTrigger value='preview'>Preview</TabsTrigger>
+              <TabsTrigger value='code'>Code</TabsTrigger>
+            </TabsList>
+            <TabsContent value='preview'>
+              <div className='flex min-h-56 items-center justify-center rounded border'>
+                <PinInput
+                  className='flex h-10 space-x-4'
+                  value={pinInput}
+                  onChange={setPinInput}
+                  onComplete={(str) => console.log('completed', str)}
+                >
+                  {Array.from({ length: 4 }, (_, i) => (
+                    <PinInputField key={i} component={Input} />
+                  ))}
+                </PinInput>
+              </div>
+            </TabsContent>
+            <TabsContent value='code'>
+              <SyntaxHighlighter
+                language='tsx'
+                style={nord}
+                wrapLines
+                wrapLongLines
+              >
+                {`function ControlledPinInput() {
+                  const [pinInput, setPinInput] = useState('');
+                
+                  return (
+                    <PinInput
+                      className='flex h-10 space-x-4'
+                      value={pinInput}
+                      onChange={setPinInput}
+                      onComplete={(str) => 
+                        console.log('completed', str)
+                      }
+                    >
+                      {Array.from({ length: 4 }, (_, i) => (
+                        <PinInputField key={i} component={Input} />
+                      ))}
+                    </PinInput>
+                  )
+                }`}
+              </SyntaxHighlighter>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+
+          <FormField
+            control={form.control}
+            name="language"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Language</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                          "w-[200px] justify-between",
+                          !field.value && "text-muted-foreground"
+                        )}
                       >
-                        {Array.from({ length: 4 }, (_, i) => (
-                          <PinInputField key={i} component={Input} />
+                        {field.value
+                          ? languages.find(
+                              (language) => language.value === field.value
+                            )?.label
+                          : "Select language"}
+                        <IconSelector className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                      <CommandInput placeholder="Search language..." />
+                      <CommandEmpty>No language found.</CommandEmpty>
+                      <CommandGroup>
+                        {languages.map((language) => (
+                          <CommandItem
+                            value={language.label}
+                            key={language.value}
+                            onSelect={() => {
+                              form.setValue("language", language.value)
+                            }}
+                          >
+                            <IconCheck
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                language.value === field.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                            {language.label}
+                          </CommandItem>
                         ))}
-                      </PinInput>
-                    )
-                  }`}
-                </SyntaxHighlighter>
-              </TabsContent>
-            </Tabs>
-          </div>
-        </div>
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  This is the language that will be used in the dashboard.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
 
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col gap-4"
-          >
-
-            <FormField
-              control={form.control}
-              name="language"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel>Language</FormLabel>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <FormControl>
-                        <Button
-                          variant="outline"
-                          role="combobox"
-                          className={cn(
-                            "w-[200px] justify-between",
-                            !field.value && "text-muted-foreground"
-                          )}
-                        >
-                          {field.value
-                            ? languages.find(
-                                (language) => language.value === field.value
-                              )?.label
-                            : "Select language"}
-                          <IconSelector className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                        </Button>
-                      </FormControl>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-[200px] p-0">
-                      <Command>
-                        <CommandInput placeholder="Search language..." />
-                        <CommandEmpty>No language found.</CommandEmpty>
-                        <CommandGroup>
-                          {languages.map((language) => (
-                            <CommandItem
-                              value={language.label}
-                              key={language.value}
-                              onSelect={() => {
-                                form.setValue("language", language.value)
-                              }}
-                            >
-                              <IconCheck
-                                className={cn(
-                                  "mr-2 h-4 w-4",
-                                  language.value === field.value
-                                    ? "opacity-100"
-                                    : "opacity-0"
-                                )}
-                              />
-                              {language.label}
-                            </CommandItem>
-                          ))}
-                        </CommandGroup>
-                      </Command>
-                    </PopoverContent>
-                  </Popover>
-                  <FormDescription>
-                    This is the language that will be used in the dashboard.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </form>
-        </Form>
-
-        
       
-      </LayoutBody>
-    </Layout>
+    
+    </LayoutBody>
   )
 }
