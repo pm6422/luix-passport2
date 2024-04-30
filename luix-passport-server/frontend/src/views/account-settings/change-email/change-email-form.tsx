06e4aa2dbb07ac607e85ef0dd84cdeb84ee1196f
louis <pm6422@126.com>
2024-04-30 11:30:50 +0800

    r

--- luix-passport-server/frontend/src/views/account-settings/change-email/change-email-form.tsx
+++ luix-passport-server/frontend/src/views/account-settings/change-email/change-email-form.tsx
@@ -1,7 +1,9 @@
+import { useState, useEffect } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/custom/button"
+import { IconReload } from "@tabler/icons-react"
 import {
   Form,
   FormControl,
@@ -11,112 +13,64 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import InputFormField from "@/components/custom/form-field/input"
 import { toast } from "sonner"
-import { useTheme } from "@/stores/theme-provider"
+import { useAuthUserProvider } from "@/stores/auth-user-provider"
+import { AccountService } from "@/services/account-service"
 
 const formSchema = z.object({
-  theme: z.enum(["light", "dark"], {required_error: "Please select a theme."})
+  currentEmail: z.string().trim().min(1, { message: "Required" }).email("Invalid email format"),
+  newEmail: z.string().trim().min(1, { message: "Required" }).email("Invalid email format"),
+  verificationCode: z.string().trim().min(1, { message: "Required" })
 })
 
 type FormSchema = z.infer<typeof formSchema>
 
 export function ChangeEmailForm() {
-  const { setTheme } = useTheme()
+  const authUserProvider = useAuthUserProvider()
+  const [saving, setSaving] = useState(false)
 
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      theme: "light"
-    }
+      currentEmail: authUserProvider.authUser.email,
+      newEmail: "",
+      verificationCode: ""
+    },
+    mode: "onBlur"
   })
+
+  useEffect(() => {
+    AccountService.getCurrentAccount().then(user => {
+      form.reset({ currentEmail: user.email })
+    })
+  }, [])
   
   function onSubmit(formData: FormSchema) {
-    toast(
-      <div>
-        <span>Applied appearance</span>
-      </div>, 
-    { duration: 5000 })
-    // set the theme
-    setTheme(formData.theme)
+
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="theme"
-          render={({ field }) => (
-            <FormItem className="space-y-1">
-              <FormLabel>Theme</FormLabel>
-              <FormDescription>
-                Select the theme for the dashboard.
-              </FormDescription>
-              <FormMessage />
-              <RadioGroup
-                onValueChange={field.onChange}
-                defaultValue={field.value}
-                className="grid max-w-md grid-cols-2 gap-8 pt-2"
-              >
-                <FormItem>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="light" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                      <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                        <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                      </div>
-                    </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Light
-                    </span>
-                  </FormLabel>
-                </FormItem>
-                <FormItem>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="dark" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                      <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                        <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                      </div>
-                    </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Dark
-                    </span>
-                  </FormLabel>
-                </FormItem>
-              </RadioGroup>
-            </FormItem>
-          )}
+        <div>
+          <h3 className="text-sm font-medium">Current Email</h3>
+          <p className="text-sm text-muted-foreground mt-2">
+            {form.getValues("currentEmail")}
+          </p>
+        </div>
+
+        <InputFormField 
+          control={form.control} 
+          name="newEmail" 
+          label="New Email" 
+          required
         />
 
-        <Button type="submit">Apply appearance</Button>
+        <Button type="submit" disabled={Object.values(form.formState.errors).length > 0 || saving}>
+          {saving ? "Saving..." : "Save"}
+          {saving && (<IconReload className="ml-1 size-4 animate-spin"/>)}
+        </Button>
       </form>
     </Form>
   )
