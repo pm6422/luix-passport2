db2976aa87da8d61f7c4d11eacbc61920be64d84
pm6422 <pm6422@126.com>
2024-05-01 20:13:16 +0800

    r

--- luix-passport-server/frontend/src/components/custom/avatar-uploader.tsx
+++ luix-passport-server/frontend/src/components/custom/avatar-uploader.tsx
@@ -1,9 +1,9 @@
+import React from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/custom/button"
 import { Input } from "@/components/ui/input"
 import { toBase64 } from "@/libs/utils"
 import { IconPencil, IconUserScan } from "@tabler/icons-react"
-import React from "react"
 
 type AvatarUploaderProps = {
 	defaultValue?: string
@@ -14,8 +14,8 @@ export function AvatarUploader({
 	defaultValue,
 	onValueChange
 }: AvatarUploaderProps) {
-	
 	const inputRef = React.useRef<HTMLInputElement>(null)
+
 	const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (e.target.files && e.target.files.length > 0) {
 			const file = e.target.files[0]
@@ -32,17 +32,17 @@ export function AvatarUploader({
 					<IconUserScan className="size-16"/>
 				</AvatarFallback>
 			</Avatar>
+
 			<Button
+				type="button"
 				variant="ghost"
 				size="icon"
 				className="rounded-full p-1 bg-secondary hover:bg-secondary-foreground/30 absolute bottom-0 right-0"
-				onClick={e => {
-					e.preventDefault()
-					inputRef.current?.click()
-				}}
+				onClick={() => inputRef.current?.click()}
 			>
 				<IconPencil className="size-5"/>
 			</Button>
+
 			<Input
 				ref={inputRef}
 				type="file"
