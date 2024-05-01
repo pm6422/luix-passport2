94730d1fcc657b305ab11600c229d6f124496215
pm6422 <pm6422@126.com>
2024-05-01 19:42:11 +0800

    r

--- luix-passport-server/frontend/src/views/developer/api-docs/index.tsx
+++ luix-passport-server/frontend/src/views/developer/api-docs/index.tsx
@@ -1,14 +1,9 @@
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card"
+import { Card } from "@/components/ui/card"
 
 export default function ApiDocs() {
   return (
     <Card>
-      <CardContent>
       <iframe src="swagger-ui/index.html" width="100%" height="900"></iframe>
-      </CardContent>
     </Card>
   )
 }
