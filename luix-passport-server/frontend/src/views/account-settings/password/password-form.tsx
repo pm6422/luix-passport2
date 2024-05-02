06c9e2550e9bc844225d003947f8bc6d2ac436bb
pm6422 <pm6422@126.com>
2024-05-02 21:11:28 +0800

    r

--- luix-passport-server/frontend/src/views/account-settings/password/password-form.tsx
+++ luix-passport-server/frontend/src/views/account-settings/password/password-form.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -7,49 +7,34 @@ import { Form } from "@/components/ui/form"
 import InputFormField from "@/components/custom/form-field/input"
 import { IconReload, IconMailForward } from "@tabler/icons-react"
 import { toast } from "sonner"
-import { useAuthUserProvider } from "@/stores/auth-user-provider"
 import { AccountService } from "@/services/account-service"
 import { getErrorMessage } from "@/libs/handle-error"
-import { isValidPhoneNumber } from "react-phone-number-input"
-import { Link } from "react-router-dom"
 
 const formSchema = z.object({
-  id: z.string().trim().min(1, { message: "Required" }),
-  username: z.string().trim().min(1, { message: "Required" }),
-  email: z.string().trim().min(1, { message: "Required" }).email("Invalid email format"),
-  mobileNo: z.string().trim().min(1, { message: "Required" }).refine(isValidPhoneNumber, { message: "Invalid phone number" }),
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
-  language: z.string().trim().min(1, { message: "Required" }),
-  locale: z.string().trim().min(1, { message: "Required" }),
-  timeZone: z.string().trim().optional(),
-  dateFormat: z.string().trim().optional(),
+  currentPassword: z.string().trim().min(1, { message: "Required" }),
+  newPassword: z.string().trim().min(1, { message: "Required" })
 })
 
 type FormSchema = z.infer<typeof formSchema>
 
 export function PasswordForm() {
-  const authUserProvider = useAuthUserProvider()
   const [saving, setSaving] = useState(false)
 
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
-    defaultValues: authUserProvider.authUser
+    defaultValues: {
+      currentPassword: "",
+      newPassword: ""
+    }
   })
-
-  useEffect(() => {
-    AccountService.getCurrentAccount().then(user => {
-      form.reset(user)
-    })
-  }, [])
 
   function onSubmit(formData: FormSchema) {
     setSaving(true)
     toast.promise(save(formData), {
-      loading: "Updating account...",
+      loading: "Updating password...",
       success: () => {
         setSaving(false)
-        return "Updated account"
+        return "Updated password"
       },
       error: (error) => {
         setSaving(false)
@@ -65,40 +50,11 @@ export function PasswordForm() {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <div>
-          <h3 className="text-sm font-medium">Username</h3>
-          <p className="text-sm text-muted-foreground mt-2">
-            {form.getValues("username")}
-          </p>
-        </div>
-
-        <div className="flex items-center justify-between">
-          <div>
-            <h3 className="text-sm font-medium">Email</h3>
-            <p className="text-sm text-muted-foreground mt-2">
-              {form.getValues("email")}
-            </p>
-          </div>
-          <Link to="/account-settings/change-email">
-            <Button type="button" variant={"link"}>
-              <IconMailForward className="mr-2 size-4"/>
-              Change Email
-            </Button>
-          </Link>
-        </div>
-
-        <div>
-          <h3 className="text-sm font-medium">Mobile No.</h3>
-          <p className="text-sm text-muted-foreground mt-2">
-            {form.getValues("mobileNo")}
-          </p>
-        </div>
-
-        <InputFormField control={form.control} name="firstName" label="First Name"/>
-        <InputFormField control={form.control} name="lastName" label="Last Name"/>
+        <InputFormField control={form.control} name="currentPassword" label="Current Password"/>
+        <InputFormField control={form.control} name="newPassword" label="New Password"/>
 
         <Button type="submit" disabled={saving}>
-          {saving ? "Updating account..." : "Update account"}
+          {saving ? "Updating password..." : "Update password"}
           {saving && (<IconReload className="ml-1 size-4 animate-spin"/>)}
         </Button>
       </form>
