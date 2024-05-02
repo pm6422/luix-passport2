f0615a39e7c68c976d90105102f67d3980c49493
pm6422 <pm6422@126.com>
2024-05-02 21:03:59 +0800

    r

--- luix-passport-server/frontend/src/views/account-settings/password/index.tsx
+++ luix-passport-server/frontend/src/views/account-settings/password/index.tsx
@@ -1,5 +1,17 @@
-export default function Password() {
-  
-  return <h1>Error Example</h1>
-}
+import { Separator } from "@/components/ui/separator"
+import { PasswordForm } from "./password-form"
 
+export default function SettingsPassword() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Change Password</h3>
+        <p className="text-sm text-muted-foreground">
+          Change your account password.
+        </p>
+      </div>
+      <Separator />
+      <PasswordForm />
+    </div>
+  )
+}
