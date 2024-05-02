af90e6a48225116ef0ecbd71d60fc541a8a7ace5
pm6422 <pm6422@126.com>
2024-05-02 21:35:56 +0800

    r

--- luix-passport-server/frontend/src/views/account-settings/change-email/index.tsx
+++ luix-passport-server/frontend/src/views/account-settings/change-email/index.tsx
@@ -5,7 +5,7 @@ export default function SettingsAppearance() {
   return (
     <div className='space-y-6'>
       <div>
-        <h3 className='text-lg font-medium'>Change email</h3>
+        <h3 className='text-lg font-medium'>Change Email</h3>
         <p className='text-sm text-muted-foreground'>
           Change your email of the application.
         </p>
