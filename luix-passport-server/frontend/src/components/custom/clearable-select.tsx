e91a6291e2dc8f3af6b2e45cad16ac10642715d0
pm6422 <pm6422@126.com>
2024-05-02 08:05:07 +0800

    r

--- luix-passport-server/frontend/src/components/custom/clearable-select.tsx
+++ luix-passport-server/frontend/src/components/custom/clearable-select.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import {
   Select,
   SelectContent,
@@ -6,11 +7,11 @@ import {
   SelectTrigger,
   SelectValue,
   SelectSeparator
-} from '@/components/ui/select'
-import { Button } from '@/components/custom/button'
-import { useState } from 'react'
+} from "@/components/ui/select"
+import { Button } from "@/components/custom/button"
 
-type EnabledSelectProps = {
+
+type Props = {
   value?: string
   onValueChange?: (value: string) => void
   className?: string
@@ -20,28 +21,28 @@ export const EnabledSelect = ({
   value, 
   onValueChange, 
   className 
-}: EnabledSelectProps) => {
+}: Props) => {
   const [open, setOpen] = useState(false)
 
   return (
   <Select value={value} onValueChange={onValueChange} open={open} onOpenChange={setOpen}>
     <SelectTrigger className={className}>
-      <SelectValue placeholder='Enabled' />
+      <SelectValue placeholder="Enabled" />
     </SelectTrigger>
     <SelectContent>
       <SelectGroup>
         {/* <SelectLabel>Enabled</SelectLabel> */}
-        <SelectItem value='true'>Yes</SelectItem>
-        <SelectItem value='false'>No</SelectItem>
+        <SelectItem value="true">Yes</SelectItem>
+        <SelectItem value="false">No</SelectItem>
       </SelectGroup>
       <SelectSeparator />
       <Button
+        type="button"
         className="w-full px-2"
         variant="secondary"
         size="sm"
         onClick={(e) => {
-          e.stopPropagation()
-          onValueChange && onValueChange('')
+          onValueChange && onValueChange("")
           setOpen(false)
         }}
       >
