78e0f0849a4a8e655aad28718e48cbd7f6c0a0dd
pm6422 <pm6422@126.com>
2024-05-03 16:04:23 +0800

    r

--- luix-passport-server/frontend/src/views/auth/sign-in.tsx
+++ luix-passport-server/frontend/src/views/auth/sign-in.tsx
@@ -2,76 +2,59 @@ import { SignInForm } from "./components/sigin-in-form"
 
 export default function SignIn() {
   return (
-    <div className="font-[sans-serif] text-[#333]">
-      <div className="min-h-screen flex fle-col items-center justify-center py-6 px-4">
-        <div className="grid md:grid-cols-2 items-center gap-10 max-w-6xl w-full">
-          <div className="max-md:text-center">
-            <h2 className="lg:text-5xl text-4xl font-extrabold lg:leading-[55px]">
-              Seamless Login for Exclusive Access
-            </h2>
-            <p className="text-sm mt-6">Immerse yourself in a hassle-free login journey with our intuitively designed login form. Effortlessly access your account.</p>
-            <p className="text-sm mt-10">Don't have an account <a href="javascript:void(0);" className="text-blue-600 font-semibold hover:underline ml-1">Register here</a></p>
+    <>
+      <div className="container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900"/>
+          <img
+            src="/assets/images/logos/dynamic-logo.svg"
+            className="relative m-auto rounded-xl"
+            width={400}
+            height={50}
+            alt="Logo"
+          />
+
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;This library has saved me countless hours of work and
+                helped me deliver stunning designs to my clients faster than
+                ever before.&rdquo;
+              </p>
+              <footer className="text-sm">Sofia Davis</footer>
+            </blockquote>
           </div>
-          <form className="space-y-6 max-w-md md:ml-auto max-md:mx-auto w-full">
-            <h3 className="text-3xl font-extrabold mb-8 max-md:text-center">
-              Sign in
-            </h3>
-            <div>
-              <input name="email" type="email" autocomplete="email" required className="bg-gray-100 w-full text-sm px-4 py-3.5 rounded-md outline-blue-600" placeholder="Email address" />
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-left">
+              <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email and password below <br />
+                to log into your account
+              </p>
             </div>
-            <div>
-              <input name="password" type="password" autocomplete="current-password" required className="bg-gray-100 w-full text-sm px-4 py-3.5 rounded-md outline-blue-600" placeholder="Password" />
-            </div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                &nbsp;
-              </div>
-              <div className="text-sm">
-                <a href="jajvascript:void(0);" className="text-blue-600 hover:text-blue-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-            <div className="!mt-10">
-              <button type="button" className="w-full shadow-xl py-2.5 px-4 text-sm font-semibold rounded text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
-                Log in
-              </button>
-            </div>
-            <p className="my-10 text-sm text-gray-400 text-center">or continue with</p>
-            <div className="space-x-6 flex justify-center">
-              <button type="button"
-                className="border-none outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30px" className="inline" viewBox="0 0 512 512">
-                  <path fill="#fbbd00"
-                    d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
-                    data-original="#fbbd00" />
-                  <path fill="#0f9d58"
-                    d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z"
-                    data-original="#0f9d58" />
-                  <path fill="#31aa52"
-                    d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z"
-                    data-original="#31aa52" />
-                  <path fill="#3c79e6"
-                    d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z"
-                    data-original="#3c79e6" />
-                  <path fill="#cf2d48"
-                    d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z"
-                    data-original="#cf2d48" />
-                  <path fill="#eb4132"
-                    d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z"
-                    data-original="#eb4132" />
-                </svg>
-              </button>
-              <button type="button"
-                className="border-none outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30px" fill="#007bff" viewBox="0 0 167.657 167.657">
-                  <path d="M83.829.349C37.532.349 0 37.881 0 84.178c0 41.523 30.222 75.911 69.848 82.57v-65.081H49.626v-23.42h20.222V60.978c0-20.037 12.238-30.956 30.115-30.956 8.562 0 15.92.638 18.056.919v20.944l-12.399.006c-9.72 0-11.594 4.618-11.594 11.397v14.947h23.193l-3.025 23.42H94.026v65.653c41.476-5.048 73.631-40.312 73.631-83.154 0-46.273-37.532-83.805-83.828-83.805z" data-original="#010002"></path>
-                </svg>
-              </button>
-            </div>
-          </form>
+            <SignInForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking login, you agree to our{" "}
+              <a
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </a>{" "}
+              and{" "}
+              <a
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
