4948c38cacbb9b9d403d1d5caf1deb5dadd7b6eb
pm6422 <pm6422@126.com>
2024-05-12 15:19:10 +0800

    r

--- luix-passport-server/frontend/src/data/date-formats.tsx
+++ luix-passport-server/frontend/src/data/date-formats.tsx
@@ -1,8 +1,8 @@
 
 export const dateFormats = [
-  { value: '1', dateTimeFormat: 'YYYY-MM-DD HH:mm:ss', label: 'YYYY-MM-DD', timeFormat: 'HH:mm:ss', sample: '2021-09-10 10:15:00' },
-  { value: '2', dateTimeFormat: 'DD MMM YYYY, HH:mm A', label: 'DD MMM YYYY', timeFormat: 'HH:mm A', sample: '10 Sep 2021, 10:15 AM' },
-  { value: '3', dateTimeFormat: 'DD/MM/YYYY, HH:mm A', label: 'DD/MM/YYYY', timeFormat: 'HH:mm A', sample: '10/09/2021, 10:15 AM' },
-  { value: '4', dateTimeFormat: 'MMM DD, YYYY HH:mm a', label: 'MMM DD, YYYY', timeFormat: 'HH:mm a', sample: 'Sep 10, 2021 10:15 am' }
+  { value: "1", label: "YYYY-MM-DD", dateTimeFormat: "YYYY-MM-DD HH:mm:ss", timeFormat: "HH:mm:ss", sample: "2021-09-10 10:15:00" },
+  { value: "2", label: "DD MMM YYYY", dateTimeFormat: "DD MMM YYYY, HH:mm A", timeFormat: "HH:mm A", sample: "10 Sep 2021, 10:15 AM" },
+  { value: "3", label: "DD/MM/YYYY", dateTimeFormat: "DD/MM/YYYY, HH:mm A", timeFormat: "HH:mm A", sample: "10/09/2021, 10:15 AM" },
+  { value: "4", label: "MMM DD, YYYY", dateTimeFormat: "MMM DD, YYYY HH:mm a", timeFormat: "HH:mm a", sample: "Sep 10, 2021 10:15 am" }
 ]
 
