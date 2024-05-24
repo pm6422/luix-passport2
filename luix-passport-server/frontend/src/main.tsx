3b76df13639032540689b28c7511790d69024964
pm6422 <pm6422@126.com>
2024-05-24 21:48:53 +0800

    refactor

--- luix-passport-server/frontend/src/main.tsx
+++ luix-passport-server/frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom/client"
+import { createRoot } from "react-dom/client"
 import { AppInfoService } from "@/services/app-info-service"
 import { AccountService } from "./services/account-service"
 import { AppInfoProvider } from "@/stores/app-info-provider"
@@ -17,7 +17,7 @@ dayjs.extend(utc)
 dayjs.extend(timezone)
 
 Promise.all([AppInfoService.load(), AccountService.getCurrentAccount()]).then(results => {
-  ReactDOM.createRoot(document.getElementById("root")!).render(
+  createRoot(document.getElementById("root")!).render(
     // <React.StrictMode>
       <AppInfoProvider defaultValue={results[0]}>
         <AuthUserProvider defaultValue={results[1]}>
