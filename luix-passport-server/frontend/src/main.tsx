4bf6f7ed372bb599cd78d940314e5493fbe343b4
pm6422 <pm6422@126.com>
2024-05-24 22:28:11 +0800

    refactor

--- luix-passport-server/frontend/src/main.tsx
+++ luix-passport-server/frontend/src/main.tsx
@@ -1,6 +1,3 @@
-import { createRoot } from "react-dom/client"
-import { AppInfoService } from "./services/app-info-service"
-import { AccountService } from "./services/account-service"
 import { AppInfoProvider } from "@/stores/app-info-provider"
 import { AuthUserProvider } from "./stores/auth-user-provider"
 import { ThemeProvider } from "@/stores/theme-provider"
@@ -9,6 +6,8 @@ import { Toaster } from "@/components/custom/toaster"
 import dayjs from "dayjs"
 import utc from "dayjs/plugin/utc"
 import timezone from "dayjs/plugin/timezone"
+import type { AppInfo } from "@/stores/app-info-provider"
+import type { AuthUser } from "@/stores/auth-user-provider"
 import router from "@/router"
 import "@/main.css"
 import "@/assets/sass/intro.scss"
@@ -16,17 +15,18 @@ import "@/assets/sass/intro.scss"
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-Promise.all([AppInfoService.load(), AccountService.getCurrentAccount()]).then(results => {
-  createRoot(document.getElementById("root")!).render(
-    // <React.StrictMode>
-      <AppInfoProvider defaultValue={results[0]}>
-        <AuthUserProvider defaultValue={results[1]}>
-          <ThemeProvider>
-            <RouterProvider router={router} />
-            <Toaster/>
-          </ThemeProvider>
+type Props = {
+    appInfo: AppInfo
+    authUser: AuthUser
+}
+
+export default function Main({ appInfo, authUser }: Props) {
+    return <AppInfoProvider defaultValue={appInfo}>
+        <AuthUserProvider defaultValue={authUser}>
+            <ThemeProvider>
+                <RouterProvider router={router}/>
+                <Toaster/>
+            </ThemeProvider>
         </AuthUserProvider>
-      </AppInfoProvider>
-    // </React.StrictMode>
-  )
-})
+    </AppInfoProvider>;
+}
